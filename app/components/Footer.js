c487ee19ea88cd92f9355ed97ab2cb665a224c33
Sonish2319 <sonish2319@gmail.com>
2025-11-03 13:44:03 +0545

    location updated

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -31,7 +31,7 @@ export default function Footer() {
           className="relative w-full md:w-1/3 h-64 rounded-lg overflow-hidden flex-shrink-0"
         >
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.5!2d85.32!3d27.71!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjfCsDQyJzQwLjAiTiA4NcKwMTknMTIuMCJF!5e0!3m2!1sen!2snp!4v1234567890"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7065.091219923657!2d85.2822474423215!3d27.70043555474583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1863d9655c01%3A0xa2f2da4ad6197fca!2sSoalteemode%2C%20Kathmandu%2044600!5e0!3m2!1sen!2snp!4v1762156636014!5m2!1sen!2snp"
             width="100%"
             height="100%"
             style={{ border: 0 }}
